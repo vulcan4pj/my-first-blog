007a1aec335a338ed0a2d476a8bb5a0616a08105
carran <carran@yescnc.co.kr>
2015-12-30 06:48:03 +0000

    test

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,22 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(
+        r'^accounts/login/',
+        'django.contrib.auth.views.login',
+        name='login',
+        kwargs={ 
+            'template_name' : 'login.html'
+        }
+    ),
+    url(
+        r'^accounts/logout/',
+        'django.contrib.auth.views.logout',
+        name='logout',
+    ),
+    url(
+        r'^$', 
+        views.main, 
+        name='main'
+    ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+# def post_list(request):
+# 	return render(request, 'blog/post_list.html', {})
+	
+def main(request):
+    return render(request, 'main.html', {})
